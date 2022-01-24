ae1b0ea7f57443e082c85ff7dfd6d2ba938c0a50
daisy <daysired345@gmail.com>
2022-01-24 14:42:58 -0500

    generated a new component, added a vehicle propperty to the VehicleComponent, and added the new component to the AppComponent template file to be displayed

---
+++ src/app/vehicles/vehicles.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicles',
+  templateUrl: './vehicles.component.html',
+  styleUrls: ['./vehicles.component.scss']
+})
+export class VehiclesComponent implements OnInit {
+vehicle = 'EcoSport';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
