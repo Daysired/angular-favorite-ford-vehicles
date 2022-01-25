de058c002fbb3f34a93cbf3a3f10775323935832
daisy <daysired345@gmail.com>
2022-01-25 15:00:49 -0500

    updated the vehiclesComponent html and VehicleDetailComponent file to bind he selectedvehicle property of the VehiclesComponent to the vehicle property of the target element, which maps to the vehicle property of the VehicleDetailComponent

--- src/app/vehicle-detail/vehicle-detail.component.ts
+++ src/app/vehicle-detail/vehicle-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Vehicle } from '../vehicle';
 
 @Component({
   selector: 'app-vehicle-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-detail.component.scss']
 })
 export class VehicleDetailComponent implements OnInit {
+@Input() vehicle?: Vehicle;
 
   constructor() { }
 
