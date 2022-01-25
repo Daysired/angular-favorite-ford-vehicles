e5da2c05f74d6c5caaeabb22b1b5857193f296b0
daisy <daysired345@gmail.com>
2022-01-25 12:22:31 -0500

    created a vehicle detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { FormsModule } from '@angular/forms';
+import { VehicleDetailComponent } from './vehicle-detail/vehicle-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehiclesComponent
+    VehiclesComponent,
+    VehicleDetailComponent
   ],
   imports: [
     BrowserModule,
