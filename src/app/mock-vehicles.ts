e8c14baadc85b1d13d972f594219f5eb49029262
daisy <daysired345@gmail.com>
2022-01-25 10:57:46 -0500

    created a mock-vehicles typescript file

---
+++ src/app/mock-vehicles.ts
@@ -0,0 +1,12 @@
+import { Vehicle } from './vehicle';
+
+export const VEHICLES: Vehicle[] = [
+{ id: 1, name: 'Ford EcoSport', year: 2021, price: 'Starting at $20,395'},
+{ id: 2, name: 'Ford Bronco', year: 2021, price: 'Starting at $28,500'},
+{ id: 1, name: 'Ford Escape', year: 2021, price: 'Starting at $25,500'},
+{ id: 1, name: 'Ford Mustang-SUV', year: 2021, price: 'Starting at $27,205'},
+{ id: 1, name: 'Ford Edge', year: 2022, price: 'Starting at $35,395'},
+{ id: 1, name: 'Ford Explorer', year: 2022, price: 'Starting at $47,245'},
+{ id: 1, name: 'Ford F-150-Lightning', year: 2022, price: 'Starting at $65,500'},
+{ id: 1, name: 'Ford Maverick', year: 2022, price: 'Starting at $19,995'}
+];
