ebc1572bda70740a894444a5c47c4c614c14bc96
daisy <daysired345@gmail.com>
2022-01-26 14:16:13 -0500

    fixed id numbers for each vehicle and changed prices

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,14 +8,14 @@ import { Vehicle } from './vehicle';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const vehicles = [
-     { id: 1, name: 'Ford EcoSport', year: 2021, price: 'Starting at $20,395'},
-     { id: 2, name: 'Ford Bronco', year: 2021, price: 'Starting at $28,500'},
-     { id: 1, name: 'Ford Escape', year: 2021, price: 'Starting at $25,500'},
-     { id: 1, name: 'Ford Mustang-SUV', year: 2021, price: 'Starting at $27,205'},
-     { id: 1, name: 'Ford Edge', year: 2022, price: 'Starting at $35,395'},
-     { id: 1, name: 'Ford Explorer', year: 2022, price: 'Starting at $47,245'},
-     { id: 1, name: 'Ford F-150-Lightning', year: 2022, price: 'Starting at $65,500'},
-     { id: 1, name: 'Ford Maverick', year: 2022, price: 'Starting at $19,995'}
+     { id: 1, name: 'Ford EcoSport', year: 2021, price: '$20,395'},
+     { id: 2, name: 'Ford Bronco', year: 2021, price: '$28,500'},
+     { id: 3, name: 'Ford Escape', year: 2021, price: '$25,500'},
+     { id: 4, name: 'Ford Mustang-SUV', year: 2021, price: '$27,205'},
+     { id: 5, name: 'Ford Edge', year: 2022, price: '$35,395'},
+     { id: 6, name: 'Ford Explorer', year: 2022, price: '$47,245'},
+     { id: 7, name: 'Ford F-150-Lightning', year: 2022, price: '$65,500'},
+     { id: 8, name: 'Ford Maverick', year: 2022, price: '$19,995'}
     ];
     return {vehicles};
   }
