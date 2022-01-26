f83f8bcc0dc78e404c2a371a6eb53594ddff533a
daisy <daysired345@gmail.com>
2022-01-25 21:46:50 -0500

    added a dashboard route and a default route in the app-routing-module.ts file and added a dashboard link to the app.component.html file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VehiclesComponent } from './vehicles/vehicles.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'vehicles', component: VehiclesComponent }
 ];
 
